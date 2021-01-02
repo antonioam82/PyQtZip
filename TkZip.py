5fc16bead21c30d30a822dd8c61bcb6570413b0f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-02 20:10:34 +0100

    Create TkZip.py

---
+++ TkZip.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox, filedialog
+import tkinter.scrolledtext as sct
+
+class zipper():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("TkZip")
+        self.window.geometry("764x316")
+
+        self.window.mainloop()
+
+if __name__=="__main__":
+    zipper()
