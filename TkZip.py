89ff2bdee8e794e3785743efc0076c14827720b0
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-04 16:06:32 +0100

    Update TkZip.py

--- TkZip.py
+++ TkZip.py
@@ -13,7 +13,8 @@ class zipper():
         self.window.geometry("764x320")
         self.canvas = Canvas(self.window)
         self.canvas.place(x=537,y=30)
-        self.Filelist = []#
+        self.Filelist = []
+        self.zip_content = []
    
         self.scrollbar = Scrollbar(self.canvas,orient=VERTICAL)
         self.scrollbar.pack(side=RIGHT,fill=Y)
