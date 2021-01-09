ebf21e1dae69a8c0212c2df943882e12f8dc06da
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2021-01-09 16:49:33 +0100

    Update TkZip2.py

--- TkZip2.py
+++ TkZip2.py
@@ -39,8 +39,7 @@ class zipper():
         self.current_dir = StringVar()
         self.currentDir = Entry(self.window,width=128,textvariable=self.current_dir)
         self.currentDir.place(x=0,y=0)
-        
-        
+
         self.file_list()
 
         self.window.mainloop()
