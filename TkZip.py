76171d333e97f4e532a0beb9c7112ea76cb8a510
Antonio Alfonso Martínez <antonioalfonsomartinez255@gmail.com>
2022-09-06 16:37:58 +0200

    Primeras importaciones

---
+++ TkZip.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import zipfile
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox, filedialog
+import tkinter.scrolledtext as sct
+import os
