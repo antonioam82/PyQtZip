b4d1cea687d6cbdb863e29947b808c1a0e9e7bb1
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-09-06 16:49:42 +0200

    Update TkZip8.py

--- TkZip8.py
+++ TkZip8.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import zipfile
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox, filedialog
+import tkinter.scrolledtext as sct
+import os
 
