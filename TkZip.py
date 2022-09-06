bd4c3b46132f6ec8e6641e7b23fccd684e6c10c2
Antonio Alfonso Martínez <antonioalfonsomartinez255@gmail.com>
2022-09-06 16:50:22 +0200

    nuevo nombre

--- TkZip.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import zipfile
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox, filedialog
-import tkinter.scrolledtext as sct
-import os
