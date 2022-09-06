bda80926df23d00c14a1afa99e1c9ebc87a792b8
Antonio Alfonso Martínez <antonioalfonsomartinez255@gmail.com>
2022-09-06 16:51:47 +0200

    comentario

---
+++ TkZip8.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import zipfile
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox, filedialog
+import tkinter.scrolledtext as sct
+import os
+
+#more code here
