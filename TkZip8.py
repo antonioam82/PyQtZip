21bfca2567a57242670e1588df1957f03304566b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2022-09-17 19:31:45 +0200

    Update TkZip8.py

--- TkZip8.py
+++ TkZip8.py
@@ -11,6 +11,11 @@ import os
 class zipper():
     def __init__(self):
         self.window = tk.Tk()
+        self.window.title("TkZip8")
+        self.window.geometry("773x320")
+        self.canvas = tk.Canvas(self.window)
+        self.canvas.place(x=537,y=30)
+        self.zip_content = []
 
 if __name__=="__main__":
     zipper()
